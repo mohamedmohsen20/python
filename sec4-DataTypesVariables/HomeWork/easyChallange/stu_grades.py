aa0e224e305dcb01a1274be8ef3149d2424d54eb
mohsen <mohamedmosen20136412@gmail.com>
2021-05-07 10:58:17 +0200

    Students grades program

---
+++ sec4-DataTypesVariables/HomeWork/easyChallange/stu_grades.py
@@ -0,0 +1,27 @@
+'''
+reads 2 students information about math exam
+For each student read: his name, id and grade
+Print the students
+Print the grades average
+
+'''
+
+std1_name = input("Enter the first student's name: ")
+std1_age = int(input("Enter the first student's ID: "))
+std1_grade = float(input("Enter the first student's grade: "))
+
+std2_name=input("Enter the second student's name: ")
+std2_age=int(input("Enter the second student's ID: "))
+std2_grade=float(input("Enter the second student's grade: "))
+
+#print all info
+print('Informat for students and their "Math" grades')
+
+print(std1_name,"(",std1_age,") got grade :",std1_grade )
+
+print(std2_name,"(",std2_age,") got grade :",std2_grade )
+
+#Print the grades average
+
+avg=std1_grade+std2_grade/2.0
+print("average = ",avg)
