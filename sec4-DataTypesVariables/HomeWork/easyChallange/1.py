112a5959cc26b34eeb18be1d7273b6160ef5168e
mohsen <mohamedmosen20136412@gmail.com>
2021-05-07 10:32:32 +0200

    sol no one for HW of week4

---
+++ sec4-DataTypesVariables/HomeWork/easyChallange/1.py
@@ -0,0 +1,11 @@
+#math operation
+#read two numbers then make + - * / operation
+
+num1=float(input("Enter first number "))
+
+num2=float(input("Enter second number "))
+
+print(num1 ,"+",num2 ,"=",num1+num2)
+print(num1 ,"-",num2 ,"=",num1-num2)
+print(num1 ,"*",num2 ,"=",num1*num2)
+print(num1 ,"/",num2 ,"=",num1/num2)
